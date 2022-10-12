c7bf99535b6ff2eb3c423bc97dcc65c89c602c38
gaetanRgd <regaud.gaetan@gmail.com>
2022-10-12 22:51:08 +0000

    o

--- lecture.h
+++ lecture.h
@@ -5,7 +5,7 @@
 #include "ruleset.h"
 #include "rules.h"
 
-Ensemble lecture(char* fichier);
+Ruletab lecture(char* fichier);
 int testFinFichier(FILE* f);
 void remplacer(char* s1, char c2, char c3, int n);
 
