79a2df57a0c2c54bb1f653ad9c8b89077e1dcc43
AlexisHamon <alexis.hamon24@gmail.com>
2022-10-14 07:06:30 +0200

    commentaire structure rule

--- datastructure/rule_struct.h
+++ datastructure/rule_struct.h
@@ -3,8 +3,15 @@
 
 #include "chainedlist.h"
 
+/// @brief Un entier qui représente l'identifiant d'une rêgle
 typedef int RuleId;
 
+/// @brief Une regle dans le makefile
+/// @param id l'identifiant de la regle
+/// @param name le nom de cette regle
+/// @param requirement une liste chainée des dépendance
+/// @param commands une liste chainée des commandes
+/// @exception ATTENTION ne pas oublier de free cette liste avec free_rule(rule)
 typedef struct {
     RuleId id;
     char* name;
