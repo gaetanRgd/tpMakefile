ae90482c885a282f775853846de957aab84118ce
AlexisHamon <alexis.hamon24@gmail.com>
2022-10-14 16:23:22 +0200

    prompt error

--- datastructure/radixlist.c
+++ datastructure/radixlist.c
@@ -18,13 +18,13 @@
 
 #include <stdlib.h>
 
-typedef int Bool;
+#include "rule_function.h"
 
 /* Complexité en espace en O(n)
  * n étant le nombre de chaînes recconues par l'arbre radix */
 typedef struct {
     void* list;
-    Bool* id;
+    Rule* rule;
     char* endkey;
 } Radix;
 
@@ -35,31 +35,56 @@ typedef struct RadixChainedList {
     Radix* tree;
 } RadixChainedList;
 
+typedef struct {
+    int size_a;
+    int size_b;
+    int size_same;
+    char* common_part;
+    char* char_a;
+    char* char_b;
+} StrCompared;
+
+
+
+StrCompared compare(char* char_a, char* char_b) {
+    StrCompared comp = {strlen(char_a), strlen(char_b), 0, NULL, NULL, NULL};
+    for (int i = 0; i < ((comp.size_a > comp.size_b) ? comp.size_a : comp.size_b); i++) {
+        
+    }
+}
 
 /* Crée une node radix
  * Complexité en O(1) */
 Radix radixcreate(void) {
-    return (Radix) {NULL, 0, NULL};
+    return (Radix) {NULL, NULL, NULL};
 }
 
 /* Ajoute la clef associée à la valeur id à l'arbre radix
  * Complexité en O(k) 
  * k étant la taille de la chaîne (clef) */
-Radix radixadd(Radix radix, unsigned char* key, unsigned int value) {
-    
+void radixadd(Radix* radix, char* key, Rule* rule) {
+    int cmp = strcmp(radix -> endkey, key);
+    if (cmp == 0) {
+        radix -> rule = rule;
+    } else if (cmp > 0) {
+
+    } else {
+
+    }
 }
 
 /* Recherche la valeur associéde à la clef dans l'arbre radix
  * Complexité en O(k) 
  * k étant la taille de la chaîne (clef) */
-Radix radixget(Radix* radix, unsigned char* key) {
+char* radixget(Radix* radix, unsigned char* key) {
     RadixChainedList* list = radix->list;
     while ((list != NULL)) {
-        if (!strcomp(list -> tree -> endkey, key)) {
+        if (!strcmp(list -> tree -> endkey, key)) {
             radix = list -> tree;
             list = radix -> list;
         }
     }
+    return NULL;
 }
 
 
